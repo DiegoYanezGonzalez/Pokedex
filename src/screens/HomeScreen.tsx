0751932b217e4c303892fcea0071b0e851d2cf68
diego <diego.y8@gmail.com>
2021-04-22 12:58:17 -0400

    imageFlatList

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View , Text, Image, FlatList} from 'react-native'
+import { View , Text, Image, FlatList, ActivityIndicator} from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { usePokemonPaginated } from '../hooks/usePokemonPaginated'
 import { styles } from '../theme/appTheme'
@@ -20,11 +20,26 @@ export const HomeScreen = () => {
            <FlatList
                data={simplePokemonList}
                keyExtractor={(pokemon)=>pokemon.id}
-               renderItem={ ( {item} ) =><Text> { item.name } </Text> }
+               showsVerticalScrollIndicator={false}
+               renderItem={ ( {item} ) =>(
+                   <Image
+                   source={{uri:item.picture}}
+                   style={{
+                       width:150,
+                       height:150
+                   }}
+                   />
+               ) }
 
            //infinityScroll
               onEndReached={loadPokemons}
               onEndReachedThreshold={0.4}
+
+              ListFooterComponent={(<ActivityIndicator 
+                style={{height:100}}  
+                size={20}
+                color="grey"
+                />)}
            />
 
              {/* <Text style={{ 
