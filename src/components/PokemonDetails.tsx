6dfa04acc21af1674ddf030fc7b7bb2508cfff92
diego <diego.y8@gmail.com>
2021-04-23 15:12:58 -0400

    DetailsPokemonImagesFull

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { PokemonFull } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
 
 
 interface Props{
@@ -14,6 +15,7 @@ export const PokemonDetails = ( {pokemon}:Props) => {
 
     return (  
         <ScrollView 
+        showsVerticalScrollIndicator={false}
         style={{...StyleSheet.absoluteFillObject
         }}
         >
@@ -31,8 +33,7 @@ export const PokemonDetails = ( {pokemon}:Props) => {
                    pokemon.types.map( ({type}) =>(
                         <Text style={{
                             ...styles.regularText
-                            ,
-                            marginRight:15
+                            
                             }} key={type.name}>
                             {type.name}
                         </Text>
@@ -43,13 +44,90 @@ export const PokemonDetails = ( {pokemon}:Props) => {
 
             {/* Ttypes */}
             <View style={{
-                ...styles.container,
-                marginTop:20
+                ...styles.container
+               
             }}> 
             <Text style={styles.title}>Sprites</Text>
 
             </View>
 
+            {/* Peso */}
+            <View style={styles.container}> 
+            <Text style={styles.title}>Peso</Text>
+            <Text style={styles.regularText}>{pokemon.weight} lb </Text>
+            </View>
+
+
+           <ScrollView 
+          // style
+          showsHorizontalScrollIndicator={false}
+           horizontal={true}
+           >
+              <FadeInImage
+              style={styles.basicSprites}
+              uri={pokemon.sprites.front_default}
+              />
+
+            <FadeInImage
+              style={styles.basicSprites}
+              uri={pokemon.sprites.back_default}
+              />
+
+                <FadeInImage
+              style={styles.basicSprites}
+              uri={pokemon.sprites.front_shiny}
+              />
+
+            <FadeInImage
+              style={styles.basicSprites}
+              uri={pokemon.sprites.back_shiny}
+              />  
+           </ScrollView>
+           {/* Habilidades */}
+
+           <View style={styles.container
+            }> 
+
+            {/* Types */}
+            <Text
+            style={styles.title}>Habilidades base</Text>
+             <View style={{flexDirection:"row"}}>
+            {
+                   pokemon.abilities.map( ({ability}) =>(
+                        <Text style={{
+                            ...styles.regularText
+                            
+                            }} key={ability.name}>
+                            {ability.name}
+                        </Text>
+                   ))
+               }
+            </View>
+        </View>
+
+        {/* Habilidades */}
+
+        <View style={styles.container
+            }> 
+
+            {/* Habilidades */}
+            <Text
+            style={styles.title}>Movimientos</Text>
+             <View style={{flexDirection:"row" , flexWrap:"wrap"}}>
+            {
+                   pokemon.moves.map( ({move}) =>(
+                        <Text style={{
+                            ...styles.regularText
+                            
+                            }} key={move.name}>
+                            {move.name}
+                        </Text>
+                   ))
+               }
+            </View>
+        </View>
+
+        
         </ScrollView>
     )
 }
@@ -60,10 +138,15 @@ const styles = StyleSheet.create({
      },
      title:{
          fontSize:22,
-         fontWeight:"bold"
+         fontWeight:"bold",
+         marginTop:20
      },
      regularText:{
          fontSize:19,
 
+     },
+     basicSprites:{
+         width:100,
+         height:100
      }
 });
