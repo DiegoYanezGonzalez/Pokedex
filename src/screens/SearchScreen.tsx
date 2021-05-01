2e8a9931abf1cf0d12d017f16feb3075787afb64
diego <diego.y8@gmail.com>
2021-05-01 13:01:57 -0400

    PreLoadInfoSearchPoke

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { View, Text, Platform } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { FlatList } from 'react-native';
+import { View, Text, Platform, ActivityIndicator } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../components/PokemonCard';
 import { SearchInput } from '../components/SearchInput';
+import { usePokemonSearch } from '../hooks/usePokemonSearch';
+import {styles as globalStyle} from '../theme/appTheme';
 
 const SearchScreen = () => {
 
 const {top} = useSafeAreaInsets();
+const {isFetching,simplePokemonList} = usePokemonSearch();
+
+if(isFetching) {
+    return(
+        <View style={styles.activityContainer}>
+            <ActivityIndicator
+            size={50}
+            color="grey"
+            />
+            <Text>Cargando...</Text>
+        </View>
+    )
+}
 
     return (
         <View style={{
@@ -16,8 +34,35 @@ const {top} = useSafeAreaInsets();
 
 
             <SearchInput />
+
+            <FlatList
+               data={simplePokemonList}
+               keyExtractor={(pokemon)=>pokemon.id}
+               showsVerticalScrollIndicator={false}
+               numColumns={ 2 }
+
+               //Header
+
+               ListHeaderComponent={(
+                <Text style={{ 
+                    ...globalStyle.title,
+                    ...globalStyle.globalMargin,
+                    paddingBottom:10
+                     }}>Pokedex</Text>
+
+               )}
+               renderItem={ ( {item} ) =>( <PokemonCard pokemon={item}  /> )}
+           />
         </View>
     )
 }
 
 export default SearchScreen;
+
+const styles = StyleSheet.create({
+    activityContainer:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+});
